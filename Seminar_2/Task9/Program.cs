a2652e563c874804d8ff6afa2c6b5fcc104ac985
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 15:41:11 +0300

    Task 9

---
+++ Seminar_2/Task9/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = Random.Shared.Next(10,100);
+Console.WriteLine(number);
+int LastDigit = number%10;
+int firstDigit = number/10;
+if (firstDigit>LastDigit)
+{
+    System.Console.WriteLine("Первая цифра больше второй. Наибольшая цифрв - "+firstDigit);               
+}
+else if (firstDigit<LastDigit)
+{
+    System.Console.WriteLine("Вторая цифра больше первой. Наибольшая цифра - "+LastDigit);
+}
+else 
+{
+    System.Console.WriteLine("Цифры равны.");
+}
