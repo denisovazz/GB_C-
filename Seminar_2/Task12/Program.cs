45041b522fc7875e13caeeacbf2845cc481c8a6e
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 16:45:19 +0300

    Task12

---
+++ Seminar_2/Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%7==0&&number%23==0)
+{
+    System.Console.WriteLine("Да");
+}
+else 
+{
+    System.Console.WriteLine("Нет");
+}
