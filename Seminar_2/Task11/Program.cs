a97f9fd91d1903f491abf6c48d5845936cb3f900
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 16:33:19 +0300

    Task11

---
+++ Seminar_2/Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int result = number1%number2;
+
+if (result==0)
+{
+    System.Console.WriteLine($"{number1}, {number2} - кратно.");
+}
+else if (result != 0)
+{
+    System.Console.WriteLine($"{number1}, {number2} - некратно, остаток {result}");
+}
