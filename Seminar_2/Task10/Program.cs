fa4eb6da54568dd26ec44e5eaba95feacb9e2964
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 16:10:32 +0300

    Task 10

---
+++ Seminar_2/Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = Random.Shared.Next(100,1000);
+Console.WriteLine(number);
+int Digit3 = number%10;
+int Digit1 = number/100;
+int Digit2 = Digit1*10;
+int result = Digit2+Digit3;
+
+System.Console.WriteLine(result);
