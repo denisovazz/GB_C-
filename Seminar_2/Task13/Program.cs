57700839345f829873ac444cc1b3410bdf1c48c5
Denisova Olga <denisovazz77@gmail.com>
2023-09-27 17:05:52 +0300

    Task13

---
+++ Seminar_2/Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите первое число:");
+int number1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1*number1==number2||number2*number2==number1)
+{
+    System.Console.WriteLine("Да");
+}
+else 
+{
+    System.Console.WriteLine("Нет");
+}
