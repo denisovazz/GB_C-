815ccf18e3128805ec35d0f11aa56c8f5ec4af09
Denisova Olga <denisovazz77@gmail.com>
2023-09-29 14:34:59 +0300

    Task 22

---
+++ Seminar_3/Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetInt(string message)
+{
+int result;
+Console.WriteLine(message);
+while (!int.TryParse(Console.ReadLine(), out result))
+{
+Console.WriteLine("Ошибка ввода. Введите число");
+}
+return result;
+}
+
+int a = GetInt("Введите число");
+int count=1;
+
+while(count <= a)
+{
+    System.Console.WriteLine(count*count);
+    count++;
+}
