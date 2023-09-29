1be3444097fe09d4957581e26dfddd0de2d3f201
Denisova Olga <denisovazz77@gmail.com>
2023-09-29 12:02:47 +0300

    Task 17

---
+++ Seminar_3/Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿System.Console.WriteLine("Введите x:");
+int x = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите y:");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("1");
+}
+
+if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("2");
+}
+
+if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("3");
+}
+
+if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("4");
+}
