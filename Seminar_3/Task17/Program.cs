250a848be880fd5396893a97d0f8444d33341551
Denisova Olga <denisovazz77@gmail.com>
2023-09-29 12:28:14 +0300

    Task 18

--- Seminar_3/Task17/Program.cs
+++ Seminar_3/Task17/Program.cs
@@ -8,17 +8,21 @@ if (x > 0 && y > 0)
     System.Console.WriteLine("1");
 }
 
-if (x < 0 && y > 0)
+else if (x < 0 && y > 0)
 {
     System.Console.WriteLine("2");
 }
 
-if (x < 0 && y < 0)
+else if (x < 0 && y < 0)
 {
     System.Console.WriteLine("3");
 }
 
-if (x > 0 && y < 0)
+else if (x > 0 && y < 0)
 {
     System.Console.WriteLine("4");
+}
+else 
+{ 
+    System.Console.WriteLine("Невозможно опраделить номер четверти точки");
 }
---
+++ Seminar_3/Task18/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.WriteLine("Введите номер четверти:");
+int num = int.Parse(Console.ReadLine());
+
+
+if (num == 1)
+{
+    System.Console.WriteLine("x > 0 && y > 0");
+}
+
+else if (num == 2)
+{
+    System.Console.WriteLine("x < 0 && y > 0");
+}
+
+else if (num == 3)
+{
+    System.Console.WriteLine("x < 0 && y < 0");
+}
+
+else if (num == 4)
+{
+    System.Console.WriteLine("x > 0 && y < 0");
+}
+else 
+{ 
+    System.Console.WriteLine("Такой четверти не существует");
+}
