1e7431278a05f61a8211a83755ad00d0098e0c37
Denisova Olga <denisovazz77@gmail.com>
2023-10-03 11:01:04 +0300

    Task24

---
+++ Seminar_4/Task24/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadNumber(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int SumFrom1ToA(int a)
+{
+    int sum = 0;
+
+    for(int i = 1; i <= a; i++)
+    { 
+        sum += i;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число:");
+int result = SumFrom1ToA(number);
+System.Console.WriteLine(result);
