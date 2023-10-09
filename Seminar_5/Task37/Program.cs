b3c4b596383cd6976dd47b84f0f48c5eede214ee
Denisova Olga <denisovazz77@gmail.com>
2023-10-09 19:23:37 +0300

    Task37

---
+++ Seminar_5/Task37/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] arr = GenerateArr(5);
+Console.WriteLine(string.Join(" ", arr));
+Console.WriteLine(string.Join(", ", NewArr(arr)));
+
+
+int [] GenerateArr(int size)
+{
+    Random random = new Random();
+    int [] arr = new int [size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(1,10);
+    }
+    return arr;
+}
+int[] NewArr(int[] arr)
+{
+    int[] newArr = new int[arr.Length/2+arr.Length%2];
+    for(int i=0; i < newArr.Length; i++)
+    {
+        
+        if(i== arr.Length-1-i)
+        newArr[i] = arr[i];
+        else{newArr[i] = arr[i] * arr[(arr.Length-1) -i];}
+    }
+    return newArr;
+}
