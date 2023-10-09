86981343e30bb68b699668536fc6530b33275948
Denisova Olga <denisovazz77@gmail.com>
2023-10-09 18:41:57 +0300

    Task3

---
+++ Seminar_5/Task33/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] GetRandomArray(int length, int leftBorder, int rightBorder)
+{
+   int[] array = new int [length];
+
+   for (int i = 0; i < array.Length; i++)
+   {
+    array[i] = Random.Shared.Next(leftBorder, rightBorder+1);
+   }
+
+   return array;
+}
+
+bool SortArray(int[] array, int num)
+{
+   for(int i = 0; i < array.Length; i++)
+   {
+    if (array[i] == num)
+    return true;
+   }
+   return false;
+}
+
+int [] myArray = GetRandomArray(10, -3, 4);
+string myArrayString = string.Join(", ", myArray);
+Console.WriteLine(myArrayString);
+bool result = SortArray(myArray, 3);
+System.Console.WriteLine(result);
