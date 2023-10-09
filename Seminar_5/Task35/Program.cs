e7d7e99e62a0970cb0d5424d754e5f9435f15a63
Denisova Olga <denisovazz77@gmail.com>
2023-10-09 20:10:42 +0300

    Task35

---
+++ Seminar_5/Task35/Program.cs
@@ -0,0 +1,28 @@
+﻿int [] arr = GenerateArr(5);
+System.Console.WriteLine(string.Join(" ", arr));
+System.Console.WriteLine(FindCount(arr));
+
+
+int [] GenerateArr(int size)
+{
+    Random random = new Random();
+    int [] arr = new int [size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(1,100);
+    }
+    return arr;
+}
+
+int FindCount(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]>=10 && arr[i] <=99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
