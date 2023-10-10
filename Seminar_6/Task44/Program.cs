f80c28b3447b17914a90b1f764bf30a2931c70b6
Denisova Olga <denisovazz77@gmail.com>
2023-10-10 12:02:04 +0300

    Task44

---
+++ Seminar_6/Task44/Program.cs
@@ -0,0 +1,25 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[num];
+Fibb(arr);
+
+void Fibb(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0||i == 1)
+        {
+            arr[i] = i;
+        }
+     
+        else
+        {
+        arr[i] = arr[i-1] + arr[i-2];
+        }
+    }
+    
+}
+
+System.Console.Write(string.Join(" ", arr));
+
+
+
