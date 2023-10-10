001fd3f77f2f88ea90f30d2e6286e1904c4531b3
Denisova Olga <denisovazz77@gmail.com>
2023-10-10 12:59:58 +0300

    Task45

---
+++ Seminar_6/Task45/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] GetRandomArray(int length, int leftBorder, int rightBorder)
+{
+   int[] array = new int [length];
+
+   for (int i = 0; i < array.Length; i++)
+   {
+    array[i] = Random.Shared.Next(leftBorder, rightBorder+1);
+   }
+
+   return array;
+}
+
+
+const int Length = 6;
+const int LeftBorder = -10;
+const int RightBorder = 10;
+int[] myArray1 = GetRandomArray(Length, LeftBorder, RightBorder);
