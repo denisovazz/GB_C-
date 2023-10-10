6fddf8992830265d786a54e873c0fe5f7ad56d5e
Denisova Olga <denisovazz77@gmail.com>
2023-10-10 09:55:16 +0300

    Task42_1

---
+++ Seminar_6/Task42_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int MyNumber = 13;
+
+void PrintBinaryView(int number)
+{
+    if (number <= 0) return;
+    PrintBinaryView(number/2);
+    System.Console.Write(number%2);
+}
+
+PrintBinaryView(MyNumber);
