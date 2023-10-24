f08937c9f54610998945aa1bbe36da6c7ea968f7
Denisova Olga <denisovazz77@gmail.com>
2023-10-24 16:17:26 +0300

    Task51

---
+++ Seminar_7/Task51/Program.cs
@@ -0,0 +1,54 @@
+﻿int sum = 0;
+int[,] GetMatrix(int rows, int colums)
+{
+    int[,] matrix = new int[rows,colums];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i+j;
+        }
+    }    
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i,j]}\t");
+           
+        }
+         System.Console.WriteLine();
+    }
+
+}
+
+int NewMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           
+           if (i==j)
+           {
+            sum = sum + matrix[i,j];
+        }
+        }
+       
+    }
+    return sum;
+}
+
+const int Rows = 3;
+const int Colums = 3;
+
+
+int[,] myMatrix = GetMatrix(Rows, Colums);
+PrintMatrix(myMatrix);
+System.Console.WriteLine(NewMatrix(myMatrix));
