d00b102bbb9e2f39bbc030b5ca8af7ac469021d3
Denisova Olga <denisovazz77@gmail.com>
2023-10-25 18:19:30 +0300

    Task65

---
+++ Seminar_9/Task65/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 10;
+int m = 4;
+
+void  PrintNatural(int n, int m)
+{
+    if (m>n)
+    {
+      return;
+    } 
+    PrintNatural(n-1, m);
+    System.Console.WriteLine(n);
+   
+}
+PrintNatural(n, m);
