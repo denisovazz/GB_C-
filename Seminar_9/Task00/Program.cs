f23e990bf7b60baf21960c7da24fb2a5177ce422
Denisova Olga <denisovazz77@gmail.com>
2023-10-25 17:46:07 +0300

    Task00

---
+++ Seminar_9/Task00/Program.cs
@@ -0,0 +1,15 @@
+﻿//при помощи рекурсии вывести факториал числа N
+
+const uint N = 5;
+
+uint Factorial(uint n)
+{
+    if(n == 0 || n == 1)
+    {
+        return 1;
+    }
+    return n * Factorial(n-1);
+}
+
+uint result = Factorial(N);
+System.Console.WriteLine(result);
