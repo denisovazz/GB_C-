6f4ee8e85c1e55ade3933f189108c43f31cdd397
Denisova Olga <denisovazz77@gmail.com>
2023-10-26 21:26:34 +0300

    Task69

---
+++ Seminar_9/Task69/Program.cs
@@ -0,0 +1,15 @@
+﻿int Pow(int a, int b)
+{
+    if (b==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return a * Pow(a, b-1);
+    }
+}
+
+int a = 5;
+int b = 3;
+System.Console.WriteLine(Pow(a,b));
